d13014647733d4818eb5ee0897432e898d0b0adc
Ben Taylor <bentaylors@hotmail.co.uk>
2026-05-09 22:59:19 +0700

    hub

--- backend/start.sh
+++ backend/start.sh
@@ -3,8 +3,5 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Install Python dependencies
-pip install -r requirements.txt
-
 # Run the Uvicorn server
-uvicorn server:app --host 0.0.0.0 --port $PORT
+uvicorn server:app --host 0.0.0.0 --port ${PORT:-8000}
