668cd1135bbe2741a5dd816b01b14c694581f035
Ben Taylor <bentaylors@hotmail.co.uk>
2026-05-11 07:49:33 +0700

    maybe this helps

---
+++ frontend/src/pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Account = () => { 
+  return ( 
+    <div> 
+      <h1>Account Page</h1> 
+      {/* Add account-related content here */} 
+    </div> 
+  ); 
+}; 
+
+export default Account;
